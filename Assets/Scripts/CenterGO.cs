18548230e86fd4845bba86a4dae7db6b436374de
Blightbuster <schevemo@katharineum.de>
2017-09-11 23:21:55 +0200

    Added: -Wood Texture -new types of Board and Scale -Script to position GameObjects in the center of the visible parent area -Collider for Sausage, Board and Scale -Ability to slice the Sausage

---
+++ Assets/Scripts/CenterGO.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterGO : MonoBehaviour
+{
+    private Vector3 lowerRight;
+
+    // Use this for initialization
+    void Start()
+	{
+		Vector3 lowerRightScreen = new Vector3(Screen.width, 0, 0); //Screen coordinate corner location
+        lowerRight = Camera.main.ScreenToWorldPoint(lowerRightScreen);  //Corner locatios in world coordinates
+
+        AlignDisplay();
+	}
+
+    private void AlignDisplay()
+    {
+        Vector3 parentPosition = transform.parent.position;
+        parentPosition -= GetComponentInParent<SpriteRenderer>().bounds.size;
+        parentPosition.y += GetComponentInParent<SpriteRenderer>().bounds.size.y * 2;
+        Vector3 parentCenter = (parentPosition + lowerRight) / 2;
+        transform.position = parentCenter;
+    }
+}
