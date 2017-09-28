6849664094680b35184cbd34d6debd534af0a959
Blightbuster <schevemo@katharineum.de>
2017-09-29 01:31:29 +0200

    Debug only feature

---
+++ Assets/Scripts/GameStateDisplay.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStateDisplay : MonoBehaviour {
+	// Update is called once per frame
+	void Update ()
+	{
+	    GetComponent<Text>().text = Manager.GameManager.State.ToString();
+	}
+}
