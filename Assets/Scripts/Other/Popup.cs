cf02f949b62b811616cbc1a9232d2f94f38b42b5
Blightbuster <schevemo@katharineum.de>
2017-11-14 01:29:11 +0100

    Added Popup script for popup controll

---
+++ Assets/Scripts/Other/Popup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game
+{
+    public class Popup : MonoBehaviour
+    {
+        public string Text = "";
+        public int DisplayTime = 3;
+
+        private Animator _animator;
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+            GetComponentInChildren<Text>().text = Text;
+            StartCoroutine(Disappear());
+        }
+
+        private IEnumerator Disappear()
+        {
+            yield return new WaitForSeconds(DisplayTime);   // Wait until set displaytime is over
+            _animator.SetFloat("Speed", -1);                 // Reverse animation
+            _animator.Play("PopupIn", -1);                   // Play the animation
+            Destroy(gameObject, _animator.GetCurrentAnimatorStateInfo(0).length + DisplayTime);    // Destroy gameobject when the animation finished
+        }
+    }
+}
