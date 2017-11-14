6cff69139a51c77a0a465bd5005983fa11a89454
Blightbuster <schevemo@katharineum.de>
2017-11-14 01:29:38 +0100

    Add Tools class for some static helperfunctions

---
+++ Assets/Scripts/Other/Tools.cs
@@ -0,0 +1,19 @@
+﻿using Game;
+using UnityEngine;
+
+namespace Other
+{
+    public class Tools
+    {
+
+        public static void CreatePopup(string text, int displayTime)
+        {
+            Canvas canvasComponent = GameObject.Find("Canvas").GetComponent<Canvas>();
+            GameObject popup = GameObject.Instantiate(Resources.Load<GameObject>("Popup"));
+            Popup popupScript = popup.GetComponent<Popup>();
+            popupScript.Text = text;
+            popupScript.DisplayTime = displayTime;
+            popup.transform.SetParent(canvasComponent.gameObject.transform, false);
+        }
+    }
+}
