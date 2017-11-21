e71b8aa74d66fe963293ec69ff2340cd362da280
Blightbuster <schevemo@katharineum.de>
2017-11-21 18:05:03 +0100

    Add privatematch submenu for joining and creating private games

---
+++ Assets/Scripts/Menu/PrivateMatch.cs
@@ -0,0 +1,18 @@
+﻿using Game;
+using Other;
+using UnityEngine;
+
+namespace Menu
+{
+    public class PrivateMatch : MonoBehaviour
+    {
+        public void JoinButton() { }
+
+        public void CreateButton() { }
+
+        public void Back()
+        {
+            Scenes.Load("Multiplayer");
+        }
+    }
+}
