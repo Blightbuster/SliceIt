8756b29268917e8b729a1fe7ebea748c01d6fbdb
Blightbuster <schevemo@katharineum.de>
2017-11-21 17:57:18 +0100

    Add loginbutton

--- Assets/Scripts/Menu/Main.cs
+++ Assets/Scripts/Menu/Main.cs
@@ -1,9 +1,20 @@
-﻿using UnityEngine;
+﻿using Game;
+using Other;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Menu
 {
     public class Main : MonoBehaviour
     {
+        public Sprite LoginSprite;
+        public Sprite LogoutSprite;
+
+        private void Start()
+        {
+            GameObject.Find("Login").GetComponent<Image>().sprite = MultiplayerManager.Instance.LoggedIn ? LogoutSprite : LoginSprite;
+        }
+
         public void PlayButton()
         {
             Scenes.Load("Gamemode");
@@ -24,5 +35,22 @@ namespace Menu
             Scenes.Load("Settings");
         }
 
+        public void LoginButton()
+        {
+            if (MultiplayerManager.Instance.LoggedIn)
+            {
+                if (MultiplayerManager.Instance.Logout())
+                {
+                    GameObject.Find("Login").GetComponent<Image>().sprite = LoginSprite;
+                }
+            }
+            else
+            {
+                if (MultiplayerManager.Instance.Login())
+                {
+                    GameObject.Find("Login").GetComponent<Image>().sprite = LogoutSprite;
+                }
+            }
+        }
     }
 }
