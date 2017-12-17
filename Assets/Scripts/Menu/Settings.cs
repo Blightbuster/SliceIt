0ece71ba5c3d836c1078661c16e3a9719a3defb9
Blightbuster <schevemo@katharineum.de>
2017-12-17 21:15:57 +0100

    Add login and register for settings menu

--- Assets/Scripts/Menu/Settings.cs
+++ Assets/Scripts/Menu/Settings.cs
@@ -1,13 +1,34 @@
-﻿using Other;
+﻿using Game;
+using Other;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Menu
 {
     public class Settings : MonoBehaviour
     {
+        public InputField Username;
+        public InputField Password;
+
+        private void Start()
+        {
+            Username.text = SecurePlayerPrefs.GetString("ClientName");
+            Password.text = SecurePlayerPrefs.GetString("ClientPassword");
+        }
+
         public void Back()
         {
             MenuManager.Instance.Load("Main");
+        }
+
+        public void LoginButton()
+        {
+            MultiplayerManager.Instance.Login(Username.text, Password.text);
+        }
+
+        public void RegisterButton()
+        {
+            MultiplayerManager.Instance.Register(Username.text, Password.text);
         }
     }
 }
