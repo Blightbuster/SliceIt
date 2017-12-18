e17c9697d2d4401be6db4398638d378d1e5e9432
Blightbuster <schevemo@katharineum.de>
2017-12-18 01:20:46 +0100

    Quickmatch button changes to waiting sprite while waiting for an opponent

--- Assets/Scripts/Menu/Multiplayer.cs
+++ Assets/Scripts/Menu/Multiplayer.cs
@@ -6,18 +6,31 @@ namespace Menu
 {
     public class Multiplayer : MonoBehaviour
     {
+        public GameObject Menu;
+
         public void QuickMatchButton()
         {
             MultiplayerManager.Instance.QuickMatch();
+            Menu.transform.Find("WaitingForOpponent").gameObject.SetActive(true);
+            Menu.transform.Find("QuickMatch").gameObject.SetActive(false);
+        }
+
+        public void CancelQuickMatchButton()
+        {
+            MultiplayerManager.Instance.CancelQuickMatch();
+            Menu.transform.Find("WaitingForOpponent").gameObject.SetActive(false);
+            Menu.transform.Find("QuickMatch").gameObject.SetActive(true);
         }
 
         public void PrivateMatchButton()
         {
+            CancelQuickMatchButton();
             MenuManager.Instance.Load("PrivateMatch");
         }
 
         public void Back()
         {
+            CancelQuickMatchButton();
             MenuManager.Instance.Load("Gamemode");
         }
     }
